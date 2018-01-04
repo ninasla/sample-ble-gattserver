0aad4fcbfaa687356b6e0536dd2581787561e2ac
Nina S. Windows <nsverdlenko@intrinsyc.com>
2018-01-03 17:34:38 -0800

    Initial commit

---
+++ app/src/main/java/com/ninasla/sample/ble_gattserver/BleServerActivity.java
@@ -0,0 +1,13 @@
+package com.ninasla.sample.ble_gattserver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BleServerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ble_server);
+    }
+}
